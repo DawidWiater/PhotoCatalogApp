18dc5d8a0419adc95fc0bae784204e9796c5a4d7
Dawid Wiater <dw118486@stud.ur.edu.pl>
2024-06-04 19:25:30 +0200

    Add project files.

---
+++ PhotoCatalogApp/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace PhotoCatalogApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
